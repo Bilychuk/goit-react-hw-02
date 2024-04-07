25f44ff62457240d84247008d3293ac4ae7846e6
Bilychuk Kateryna <bilychukkateryna@ukr.net>
2024-04-07 13:58:50 +0300

    fix options

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,60 +1,34 @@
 import css from './Options.module.css';
 
 export default function Options({ updateFeedback, total, reset }) {
-  if (total === 0) {
-    return (
-      <div className={css.list}>
-        <button
-          className={css.button}
-          type="button"
-          onClick={() => updateFeedback('good')}
-        >
-          Good
-        </button>
-        <button
-          className={css.button}
-          type="button"
-          onClick={() => updateFeedback('neutral')}
-        >
-          Neutral
-        </button>
-        <button
-          className={css.button}
-          type="button"
-          onClick={() => updateFeedback('bad')}
-        >
-          Bad
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <div className={css.list}>
-        <button
-          className={css.button}
-          type="button"
-          onClick={() => updateFeedback('good')}
-        >
-          Good
-        </button>
-        <button
-          className={css.button}
-          type="button"
-          onClick={() => updateFeedback('neutral')}
-        >
-          Neutral
-        </button>
-        <button
-          className={css.button}
-          type="button"
-          onClick={() => updateFeedback('bad')}
-        >
-          Bad
-        </button>
+  return (
+    <div className={css.list}>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => updateFeedback('good')}
+      >
+        Good
+      </button>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => updateFeedback('neutral')}
+      >
+        Neutral
+      </button>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => updateFeedback('bad')}
+      >
+        Bad
+      </button>
+      {total > 0 && (
         <button className={css.button} type="button" onClick={reset}>
           Reset
         </button>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
